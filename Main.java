9ce4f0628e75f53b061a8edfc78730f6bc054ade
Talita de Souza Keller <142261913+MintKeller@users.noreply.github.com>
2025-08-18 22:16:59 -0300

    Add files via upload
    1st

---
+++ Main.java
@@ -0,0 +1,143 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+
+    public static void main(String[] args) {
+        List<Question> questions = new ArrayList<>();
+
+        Question q1 = new Question(1, "You regularly make new friends.", "");
+        Question q2 = new Question(2, "Complex and novel ideas excite you more than simple and straightforward ones", "");
+        Question q3 = new Question(3, "You usually feel more persuaded by what resonates emotionally with you than by factual arguments.", "");
+        Question q4 = new Question(4, "Your living and working spaces are clean and organized.", "");
+        Question q5 = new Question(5, "You usually stay calm, even under a lot of pressure.", "");
+        Question q6 = new Question(6, "You find the idea of networking or promoting yourself to strangers very daunting.", "");
+        Question q7 = new Question(7, "You prioritize and plan tasks effectively, often completing them well before the deadline.", "");
+        Question q8 = new Question(8, "People’s stories and emotions speak louder to you than numbers or data.", "");
+        Question q9 = new Question(9, "You like to use organizing tools like schedules and lists.", "");
+        Question q10 = new Question(10, "Even a small mistake can cause you to doubt your overall abilities and knowledge.", "");
+        Question q11 = new Question(11, "You feel comfortable just walking up to someone you find interesting and striking up a conversation.", "");
+        Question q12 = new Question(12, "You are not too interested in discussions about various interpretations of creative works.", "");
+        Question q13 = new Question(13, "You prioritize facts over people’s feelings when determining a course of action.", "");
+        Question q14 = new Question(14, "You often allow the day to unfold without any schedule at all.", "");
+        Question q15 = new Question(15, "You rarely worry about whether you make a good impression on people you meet.", "");
+        Question q16 = new Question(16, "You enjoy participating in team-based activities.", "");
+        Question q17 = new Question(17, "You enjoy experimenting with new and untested approaches.", "");
+        Question q18 = new Question(18, "You prioritize being sensitive over being completely honest.", "");
+        Question q19 = new Question(19, "You actively seek out new experiences and knowledge areas to explore.", "");
+        Question q20 = new Question(20, "You are prone to worrying that things will take a turn for the worse.", "");
+        Question q21 = new Question(21, "You enjoy solitary hobbies or activities more than group ones.", "");
+        Question q22 = new Question(22, "You cannot imagine yourself writing fictional stories for a living.", "");
+        Question q23 = new Question(23, "You favor efficiency in decisions, even if it means disregarding some emotional aspects.", "");
+        Question q24 = new Question(24, "You prefer to do your chores before allowing yourself to relax.", "");
+        Question q25 = new Question(25, "You usually wait for others to introduce themselves first at social gatherings.", "");
+        Question q26 = new Question(26, "Your mood can change very quickly.", "");
+        Question q27 = new Question(27, "You are not easily swayed by emotional arguments.", "");
+        Question q28 = new Question(28, "You often end up doing things at the last possible moment.", "");
+        Question q29 = new Question(29, "You enjoy debating ethical dilemmas.", "");
+        Question q30 = new Question(30, "You usually prefer to be around others rather than on your own.", "");
+        Question q31 = new Question(31, "You become bored or lose interest when the discussion gets highly theoretical.", "");
+        Question q32 = new Question(32, "When facts and feelings conflict, you usually find yourself following your heart.", "");
+        Question q33 = new Question(33, "You find it challenging to maintain a consistent work or study schedule.", "");
+        Question q34 = new Question(34, "You rarely second-guess the choices that you have made.", "");
+        Question q35 = new Question(35, "You rarely second-guess the choices that you have made.", "");
+        Question q36 = new Question(36, "In disagreements, you prioritize proving your point over preserving the feelings of others.", "");
+        Question q37 = new Question(37, "You are drawn to various forms of creative expression, such as writing.", "");
+        Question q38 = new Question(38, "You usually base your choices on objective facts rather than emotional impressions.", "");
+        Question q39 = new Question(39, "You like to have a to-do list for each day.", "");
+        Question q40 = new Question(40, "You rarely feel insecure.", "");
+        Question q41 = new Question(41, "You avoid making phone calls.", "");
+        Question q42 = new Question(42, "You enjoy exploring unfamiliar ideas and viewpoints.", "");
+        Question q43 = new Question(43, "You can easily connect with people you have just met.", "");
+        Question q44 = new Question(44, "If your plans are interrupted, your top priority is to get back on track as soon as possible.", "");
+        Question q45 = new Question(45, "You are still bothered by mistakes that you made a long time ago.", "");
+        Question q46 = new Question(46, "You are not too interested in discussing theories on what the world could look like in the future.", "");
+        Question q47 = new Question(47, "Your emotions control you more than you control them.", "");
+        Question q48 = new Question(48, "When making decisions, you focus more on how the affected people might feel than on what is most logical or efficient.", "");
+        Question q49 = new Question(49, "Your personal work style is closer to spontaneous bursts of energy than organized and consistent efforts.", "");
+        Question q50 = new Question(50, "When someone thinks highly of you, you wonder how long it will take them to feel disappointed in you.", "");
+        Question q51 = new Question(51, "You would love a job that requires you to work alone most of the time.", "");
+        Question q52 = new Question(52, "You believe that pondering abstract philosophical questions is a waste of time.", "");
+        Question q53 = new Question(53, "You feel more drawn to busy, bustling atmospheres than to quiet, intimate places.", "");
+        Question q54 = new Question(54, "If a decision feels right to you, you often act on it without needing further proof.", "");
+        Question q55 = new Question(55, "You often feel overwhelmed.", "");
+        Question q56 = new Question(56, "You complete things methodically without skipping over any steps.", "");
+        Question q57 = new Question(57, "You prefer tasks that require you to come up with creative solutions rather than follow concrete steps.", "");
+        Question q58 = new Question(58, "You are more likely to rely on emotional intuition than logical reasoning when making a choice.", "");
+        Question q59 = new Question(59, "You struggle with deadlines.", "");
+        Question q60 = new Question(60, "You feel confident that things will work out for you.", "");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Question.java
@@ -0,0 +1,30 @@
+//maybe it should be an affirmation, but k
+public class Question {
+    private int id;
+    private String text;
+    private String pillar;
+
+    public Question (int id, String text, String pillar) {
+        this.id = id;
+        this.text = text;
+        this.pillar = pillar;
+    }
+
+    //Getters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public String getPillar() {
+        return pillar;
+    }
+
+
+
+
+
+}
